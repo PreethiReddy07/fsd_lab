88240268a90bfa58122929e284c441227a31d24f
PreethiReddy07 <preethireddy762005@gmail.com>
2025-02-19 11:22:33 +0530

    lab-6 intial commit

---
+++ Lab-6/ques3.js
@@ -0,0 +1,26 @@
+// Question 2: Write a Node.js server that parses query parameters from a GET 
+// request. For example, if the request is /greet?name=John, the server should 
+// respond with "Hello, John!".
+
+
+const http = require('http');
+const url = require('url');
+const server = http.createServer((req, res) => {
+    const parurl = url.parse(req.url, true);
+    if (parurl.pathname === '/greet') {
+        const name = parurl.query.name;
+        if (name) {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(`<h3>Hello, ${name}!</h3>`);
+        } else {
+            res.writeHead(400, { 'Content-Type': 'text/plain' });
+            res.end('Name query parameter is missing');
+        }
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Page not found');
+    }
+});
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
